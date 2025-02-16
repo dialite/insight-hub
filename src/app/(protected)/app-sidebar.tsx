425f42a7bb125a9587d2eae0c1fdbd97801b3f0c
dialite <raymondolisa01@gmail.com>
2025-02-16 22:05:03 +0100

    Render the menu side bar options

--- src/app/(protected)/app-sidebar.tsx
+++ src/app/(protected)/app-sidebar.tsx
@@ -1,7 +1,67 @@
 "use client"
 
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
+import { cn } from "@/lib/utils";
+import { Bot, CreditCard, LayoutDashboard, Presentation } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const items = [
+    {
+        title: "Dashboard",
+        url: "/dashboard",
+        icon: LayoutDashboard
+    },
+    {
+        title: "Q&A",
+        url: "/qa",
+        icon: Bot
+    },
+    {
+        title: "Meetings",
+        url: "/meetings",
+        icon: Presentation
+    },
+    {
+        title: "Billing",
+        url: "/billing",
+        icon: CreditCard
+    }
+]
+
 export function AppSidebar() {
+    const pathName = usePathname()
     return (
-        <div>Sidebar here</div>
+        <Sidebar collapsible="icon" variant="floating">
+            <SidebarHeader>
+                Logo
+            </SidebarHeader>
+
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupLabel>
+                        Application
+                    </SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {items.map(item=> {
+                                return (
+                                    <SidebarMenuItem key={item.title}>
+                                        <SidebarMenuButton asChild>
+                                            <Link href={item.url} className={cn(pathName === item.url ? "bg-primary text-white" : "", "list-none")}
+                                            >
+                                                {React.createElement(item.icon)}
+                                                <span>{item.title}</span>
+                                            </Link>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuItem>
+                                )
+                            })}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+        </Sidebar>
     )
 }

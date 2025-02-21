5451025aa0191325d0d276a8bba742e6edbd6ab8
dialite <raymondolisa01@gmail.com>
2025-02-21 10:38:29 +0100

    Dashboard ask question card interface

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -5,6 +5,7 @@ import { ExternalLink, ExternalLinkIcon, Github } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react';
 import CommitLog from './commit-log';
+import AskQuestionCard from './ask-question-card';
 
 function DashboardPage() {
     const {project} = useProject()
@@ -39,7 +40,7 @@ function DashboardPage() {
 
       <div className='mt-4'>
         <div className='grid grid-cols-1 gap-4 sm:grid-cols-5'>
-          AskQuestionCard
+          <AskQuestionCard />
           MeetingCard
         </div>
       </div>

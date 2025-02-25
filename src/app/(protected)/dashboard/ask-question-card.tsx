5fef93bfd35e83744668d233540ae506e8873c2b
dialite <raymondolisa01@gmail.com>
2025-02-25 07:21:54 +0100

    Implement the markdown editor and syntax highlighter for better text rich content rendering

--- src/app/(protected)/dashboard/ask-question-card.tsx
+++ src/app/(protected)/dashboard/ask-question-card.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import MDEditor from "@uiw/react-md-editor"
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
@@ -9,6 +10,7 @@ import Image from 'next/image';
 import React from 'react';
 import { askQuestion } from './actions';
 import { readStreamableValue } from 'ai/rsc';
+import CodeReferences from "./code-references";
 
 const AskQuestionCard = () => {
     const {project} = useProject()
@@ -19,11 +21,14 @@ const AskQuestionCard = () => {
     const [answer, setAnswer] = React.useState("")
 
     const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        setAnswer("")
+        setFilesReferences([])
         e.preventDefault()
         if (!project?.id) return
-        setOpen(true)
+        setLoading(true)
 
         const {output, filesReferences} = await askQuestion(question, project.id)
+        setOpen(true)
         setFilesReferences(filesReferences)
 
         for await (const delta of readStreamableValue(output)) {
@@ -37,17 +42,21 @@ const AskQuestionCard = () => {
   return (
     <>
         <Dialog open={open} onOpenChange={setOpen}>
-            <DialogContent>
+            <DialogContent className="sm:max-w-[80vw]">
                 <DialogHeader>
                     <DialogTitle>
                         <Image src="/logo.png" alt="insighthub" width={40} height={40}/>
                     </DialogTitle>
                 </DialogHeader>
-                {answer}
-                <h1>Files References</h1>
-                {filesReferences.map(file => {
-                    return <span key={file.fileName}>{file.fileName}</span>
-                })}
+
+                <MDEditor.Markdown source={answer} className="max-w-[70vw] lh-full max-h-[40vh] overflow-scroll"/>
+                <div className="h-4"></div>
+                <CodeReferences filesReferences={filesReferences}/>
+
+                <Button type="button" onClick={() => {setOpen(false)}}>
+                    Close
+                </Button>
+
             </DialogContent>
         </Dialog>
         <Card className='relative col-span-3'>
@@ -60,7 +69,7 @@ const AskQuestionCard = () => {
                 <form onSubmit={onSubmit}>
                     <Textarea placeholder='Which file should I edit to change the home page' value={question} onChange={e => setQuestion(e.target.value)}/>
                     <div className='h-4'></div>
-                    <Button type='submit'>
+                    <Button type='submit' disabled={loading}>
                         Ask InsightHub
                     </Button>
                 </form>
